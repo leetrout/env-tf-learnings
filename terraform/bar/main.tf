76a19fb9039791ec2a99dfa3e4d938417d69563a
Lee Trout <lee@zenput.com>
2021-06-14 14:47:25 -0400

    Add bar

---
+++ terraform/bar/main.tf
@@ -0,0 +1,20 @@
+variable "pet_words" {
+  type = number
+  default = 2
+}
+
+variable "seed" {
+    type = number
+    default = 0
+}
+
+resource "random_pet" "lees_favorite" {
+  length = var.pet_words
+  keepers = {
+      seed = var.seed
+  }
+}
+
+output "lees_pet_name" {
+  value = random_pet.lees_favorite.id
+}
