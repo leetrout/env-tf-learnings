3a7e960f9873454426bc8a6a31244038c658b3e9
Lee Trout <lee@zenput.com>
2021-06-22 16:57:42 -0400

    Use remote state

--- terraform/foo/main.tf
+++ terraform/foo/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "zenput-testing-env0-state"
+    key    = "mystate"
+    region = "us-east-2"
+  }
+}
+
 variable "seed" {
     type = number
     default = 4
